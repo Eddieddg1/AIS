1820c1745506e3b9051627c4534e1ccf20508e2c
Eddieddg1 <lol.bender@outlook.com>
2024-06-16 07:42:57 +0200

    Update OneScriptTest.sh

--- OneScriptTest.sh
+++ OneScriptTest.sh
@@ -43,10 +43,9 @@ mount /dev/${DISK}${PART_SUFFIX}3 /mnt
 mkdir -p /mnt/boot
 mount /dev/${DISK}${PART_SUFFIX}1 /mnt/boot
 
-BASE_PACKAGES="base linux linux-firmware fastfetch htop nano thunderbird konsole vlc kate git"
-echo "pacstrap -K /mnt ${BASE_PACKAGES}"
-read -p "Do you want to add any additional packages? (space-separated): " ADDITIONAL_PACKAGES
-pacstrap -K /mnt ${BASE_PACKAGES} ${ADDITIONAL_PACKAGES}
+echo "pacstrap -K /mnt base linux linux-firmware fastfetch htop nano thunderbird konsole vlc kate git"
+read -p "Do you want to add any additional packages? (space-separated): " PAC
+pacstrap -K /mnt base linux linux-firmware fastfetch htop nano thunderbird konsole vlc kate git ${PAC}
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
