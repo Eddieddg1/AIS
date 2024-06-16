18648dc9ef49b0cb5335521837e2a894cd1fb829
Eddieddg1 <lol.bender@outlook.com>
2024-06-17 01:04:56 +0200

    Update OneScriptTest.sh

--- OneScriptTest.sh
+++ OneScriptTest.sh
@@ -44,6 +44,7 @@ mkfs.ext4 /dev/${DISK}${PART_SUFFIX}3
 mount /dev/${DISK}${PART_SUFFIX}3 /mnt
 mkdir -p /mnt/boot
 mount /dev/${DISK}${PART_SUFFIX}1 /mnt/boot
+
 echo "pacstrap -K /mnt base linux linux-firmware fastfetch htop nano thunderbird konsole vlc kate git sddm networkmanager systemctl"
 read -p "Do you want to add any packages?: " PAC
 pacstrap -K /mnt --confirm base linux linux-firmware fastfetch htop nano thunderbird konsole vlc kate git sddm networkmanager systemctl ${PAC}
