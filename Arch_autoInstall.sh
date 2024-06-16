5b741e8be87e721bb1517c825fc94497146b519a
Eddieddg1 <lol.bender@outlook.com>
2024-06-16 05:59:07 +0200

    Hello :)

---
+++ Arch_autoInstall.sh
@@ -0,0 +1,71 @@
+read -p "Enter Region/City for timezone (e.g., Europe/Berlin): " TIMEZONE
+ln -sf /usr/share/zoneinfo/\$TIMEZONE /etc/localtime
+hwclock --systohc
+
+locale-gen
+read -p "Please select language. en_US.UTF-8: " LANG
+if [[ -z "\$LANG" ]]; then
+    LANG="en_US.UTF-8"
+fi
+echo "LANG=\$LANG" > /etc/locale.conf
+echo "\$LANG UTF-8" > /etc/locale.gen
+locale-gen
+
+read -p "Please select keyboard layout. sv-latin1: " KEYMAP
+if [[ -z "\$KEYMAP" ]]; then
+    KEYMAP="sv-latin1"
+fi
+echo "KEYMAP=\$KEYMAP" > /etc/vconsole.conf
+
+mkinitcpio -P
+
+echo "Enter root password"
+passwd
+
+echo "Boot loaders: (~)EFISTUB, (X)Unified kernel image, (~)GRUB, (X)Limine, (X)rEFInd, (X)Syslinux, (X)systemd-boot"
+echo "(X) = Not currently supported, (O) = Supported, (~) Work In Progress"
+read -p "Please make sure to spell it correctly: " boot
+case "\$boot" in
+    "EFISTUB")
+        pacman -S --noconfirm efibootmgr
+        umount /dev/${DISK}${PART_SUFFIX}1
+        mkdir -p /boot/efi
+        mount /dev/${DISK}${PART_SUFFIX}1 /boot/efi
+        efibootmgr --create --disk /dev/${DISK} --part 1 --label "Arch Linux" --loader /vmlinuz-linux --unicode 'root=/dev/${DISK}${PART_SUFFIX}3 rw initrd=\initramfs-linux.img'
+        ;;
+#    "Unified kernel image")
+#        umount /dev/${DISK}${PART_SUFFIX}1
+#        mount --mkdir /dev/${DISK}${PART_SUFFIX}1 /boot/efi
+#        ;;
+    "GRUB")
+        pacman -S --noconfirm grub efibootmgr
+        umount /dev/${DISK}${PART_SUFFIX}1
+        mkdir -p /boot/efi
+        mount /dev/${DISK}${PART_SUFFIX}1 /boot/efi
+        grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+        grub-mkconfig -o /boot/grub/grub.cfg
+        ;;
+#    "Limine")
+#        sudo pacman -S limine
+#        ;;
+#    "rEFInd")
+#        sudo pacman -S refind
+#        ;;
+#    "Syslinux")
+#        sudo pacman -S syslinux
+#        syslinux-install_update -i -a -m
+#        ;;
+#    "systemd-boot")
+#        sudo pacman -S bootctl
+#        bootctl install
+#        ;;
+    *)
+        echo "Boot loader '\$boot' not recognized or not supported."
+        exit 1
+        ;;
+esac
+
+read -p "Do you want Awesome? (O for awesome): " AWESOME
+if [[ $AWESOME == O* ]]; then
+    pacman -S awesome
+fi
---
+++ OneScriptTest.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+timedatectl
+echo "Available disks:"
+fdisk -l | grep "Disk /dev/"
+read -p "Enter the disk to partition (e.g., sda, nvme0n1): " DISK
+
+if [[ $DISK == nvme* ]]; then
+    PART_SUFFIX="p"
+else
+    PART_SUFFIX=""
+fi
+
+read -p "Enter swap size in GB (4 or more for swap, less for no swap): " SWAP
+
+(
+echo g
+echo n
+echo
+echo
+echo +1G
+if [[ $SWAP -ge 4 ]]; then
+    echo n
+    echo
+    echo
+    echo +${SWAP}G
+fi
+echo n
+echo
+echo
+echo
+echo w
+) | fdisk /dev/$DISK
+
+mkfs.fat -F 32 /dev/${DISK}${PART_SUFFIX}1
+if [[ $SWAP -ge 4 ]]; then
+    mkswap /dev/${DISK}${PART_SUFFIX}2
+    swapon /dev/${DISK}${PART_SUFFIX}2
+fi
+mkfs.ext4 /dev/${DISK}${PART_SUFFIX}3
+
+mount /dev/${DISK}${PART_SUFFIX}3 /mnt
+mkdir -p /mnt/boot
+mount /dev/${DISK}${PART_SUFFIX}1 /mnt/boot
+
+BASE_PACKAGES="base linux linux-firmware fastfetch htop nano thunderbird konsole vlc kate git"
+echo "pacstrap -K /mnt ${BASE_PACKAGES}"
+read -p "Do you want to add any additional packages? (space-separated): " ADDITIONAL_PACKAGES
+pacstrap -K /mnt ${BASE_PACKAGES} ${ADDITIONAL_PACKAGES}
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt /bin/bash <<EOF_CHROOT
+
+EOF_CHROOT
+
+umount -R /mnt
+reboot
