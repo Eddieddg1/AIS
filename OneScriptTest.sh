013c1b8d15cc420efe2aa7fbd9a6fcac4a166d84
Eddieddg1 <lol.bender@outlook.com>
2024-06-17 01:55:02 +0200

    Update OneScriptTest.sh

--- OneScriptTest.sh
+++ OneScriptTest.sh
@@ -51,7 +51,7 @@ echo "pacstrap -K /mnt base linux linux-firmware fastfetch htop nano thunderbird
 read -p "Do you want to add any packages?: " PAC
 pacstrap -K /mnt --confirm base base-devel linux linux-firmware fastfetch htop nano thunderbird konsole vlc kate git sddm networkmanager systemctl ${PAC}
 
-genfstab -U /mnt >> /mnt/etc/fstab#
+genfstab -U /mnt >> /mnt/etc/fstab
 
 cp ./Arch_autoInstall.sh /mnt
 chmod +x /mnt/Arch_autoInstall.sh
